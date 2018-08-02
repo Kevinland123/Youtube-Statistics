126b860f58e26b6194b139644ad9ea2700803922
Kevin Zhu <Kevin Zhu@128.195.65.104>
2018-08-02 11:33:34 -0700

    hmm

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ name = input("Enter file:")
 if len(name) < 1 : 
     name = "history.html"
 
-file = open(name, encoding="utf8")
+file = open(name, encoding="UTF-8")
 
 #f=codecs.open(name, 'r')
 #print(f.read())
