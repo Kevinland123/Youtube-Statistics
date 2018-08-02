eb739ec1963eae4badba8dc5fc7765f596e07f9b
Kevin Zhu <Kevin Zhu@128.195.65.104>
2018-08-02 11:19:14 -0700

    1st changes

--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
 print("Alright we got this!")
+#import codecs
+
+name = input("Enter file:")
+if len(name) < 1 : 
+    name = "history.html"
+
+file = open(name, encoding="utf8")
+
+#f=codecs.open(name, 'r')
+#print(f.read())
+
+record = dict()
+
+for line in file:
+    print(line)
