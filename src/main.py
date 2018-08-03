b86ddabf8558e256cc199183ef5c5bad211a4300
Kevin Zhu <Kevin Zhu@KevinlandPC>
2018-08-03 13:13:02 -0700

    Output file

--- src/main.py
+++ src/main.py
@@ -1,23 +1,31 @@
 print("Alright we got this!")
-#import codecs
+# import codecs
 
 name = input("Enter file:")
 if len(name) < 1 : 
     name = "history3.html"
 
-file = open(name, encoding="UTF-8")
-
-#f=codecs.open(name, 'r')
-#print(f.read())
+fileRead = open(name, encoding="UTF-8")
+fileGenerated = open('ListOfVideo.txt', 'w')
 
 record = dict()
 videoCount = 0
+videoSkipped = 0
 
-for lines in file:
+for lines in fileRead:
     line = lines.encode("utf-8")
     new = line.decode().split()
     for x in new:
-        if 'href="https://www.youtube.com/watch' in x:
-            print(x.encode("utf-8"))
-            videoCount = videoCount + 1
-print(videoCount)
+        try:
+            if 'href="https://www.youtube.com/watch' in x:
+                # print(x.encode("utf-8"))
+                # fileGenerated.write(x.encode("utf-8"))
+                fileGenerated.write(x + "\n")
+                videoCount = videoCount + 1
+        except:
+            #print("Nope")
+            videoSkipped = videoSkipped + 1
+print("Videos counted:",  videoCount)
+print("Videos skipped:", videoSkipped)
+
+fileGenerated.close()
