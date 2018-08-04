27f338c616b21b9ff3073e352f39bac1682ad8cb
Kevin Zhu <Kevin Zhu@KevinlandPC>
2018-08-03 21:39:39 -0700

    hi

--- src/main.py
+++ src/main.py
@@ -1,33 +1,46 @@
+import pafy
+
 print("Alright we got this!")
-# import codecs
 
 name = input("Enter file:")
 if len(name) < 1 : 
-    name = "history3.html"
+    name = "history2.html"
 
 fileRead = open(name, encoding="UTF-8")
 fileGenerated = open('ListOfVideo.txt', 'w')
 
 record = dict()
+
 videoCount = 0
 videoSkipped = 0
+videoProcessed = 0
 
 for lines in fileRead:
     line = lines.encode("utf-8")
     new = line.decode().split()
+    totalVideos = len(new)
     for x in new:
         try:
             if 'href="https://www.youtube.com/watch' in x:
-                index = x.find('?')
+                index = x.find('=')
                 end = x.find('>')
-                videoID = x[index+3:end-1]
+                url = x[index+2:end-1]
+                
+                video = pafy.new(url)
+                #print video.length
                 
                 #fileGenerated.write(x + "\n")
-                fileGenerated.write(videoID + "\n")
+                fileGenerated.write(video.length + "\n")
                 videoCount = videoCount + 1
+                videoProcessed = videoProcessed + 1
+                print("Video", videoProcessed, "out of", totalVideos, "videos processed")
         except:
             #print("Nope")
             videoSkipped = videoSkipped + 1
+            videoProcessed = videoProcessed + 1
+            print("Video", videoProcessed, "out of", totalVideos, "videos processed")
+    
+    
 print("Videos counted:", videoCount)
 print("Videos skipped:", videoSkipped)
 
