0b1d1bd1133837acd7869cb492d928fd27ad18e2
Kevin Zhu <Kevin Zhu@KevinlandPC>
2018-08-04 01:05:41 -0700

    Now the total video is correct

--- src/main.py
+++ src/main.py
@@ -8,6 +8,11 @@ print("Alright we got this!")
 name = input("Enter file:")
 if len(name) < 1 : 
     name = "history.html"
+    
+try:
+    retentionFactor = float(input("Enter retention factor: (Ex: 0.8)"))
+except:
+    retentionFactor = 0.8
 
 fileRead = open(name, encoding="UTF-8")
 fileGenerated = open('ListOfVideo.txt', 'w')
@@ -17,56 +22,52 @@ record = dict()
 videoCount = 0
 videoSkipped = 0
 videoProcessed = 0
+videoList = []
 
 totalTimeSeconds = 0
-try:
-    retentionFactor = float(input("Enter retention factor: (Ex: 0.8)"))
-except:
-    retentionFactor = 0.8
 
 for lines in fileRead:
     line = lines.encode("utf-8")
     new = line.decode().split()
-    totalVideos = len(new)
+    totalVideoCount = len(new)
     for x in new:
         try:
             if 'href="https://www.youtube.com/watch' in x:
                 index = x.find('?')
                 end = x.find('>')
                 videoID = x[index+3:end-1]
-                
-                api_key = "AIzaSyCAExHHkEP_iB9RYmHXlaXC-pjPkj9RWQw"
-                searchUrl = "https://www.googleapis.com/youtube/v3/videos?id=" + videoID + "&key=" + api_key + "&part=contentDetails"
-                response = urllib.request.urlopen(searchUrl)
-                bla = response.read()
-                encoding = response.info().get_content_charset('utf-8')
-                data = json.loads(bla.decode(encoding))
-                all_data = data['items']
-                contentDetails = all_data[0]['contentDetails']
-                duration = contentDetails['duration']
-                
-                duration = isodate.parse_duration(duration)
-                durationseconds = duration.total_seconds()
-                video_dur = str(datetime.timedelta(seconds=durationseconds))
-                print(video_dur)
-                
-                totalTimeSeconds = totalTimeSeconds + durationseconds
+                videoList.append(videoID)
                 
                 videoCount = videoCount + 1
-                videoProcessed = videoProcessed + 1
-                print("Video", videoProcessed, "out of", totalVideos, "videos processed")
         except:
             print("Nope")
             videoSkipped = videoSkipped + 1
-            videoProcessed = videoProcessed + 1
-            print("Video", videoProcessed, "out of", totalVideos, "videos processed")
-    
+            
+for videos in videoList:
+    api_key = "AIzaSyCAExHHkEP_iB9RYmHXlaXC-pjPkj9RWQw"
+    searchUrl = "https://www.googleapis.com/youtube/v3/videos?id=" + videos + "&key=" + api_key + "&part=contentDetails"
+    response = urllib.request.urlopen(searchUrl)
+    bla = response.read()
+    encoding = response.info().get_content_charset('utf-8')
+    data = json.loads(bla.decode(encoding))
+    all_data = data['items']
+    contentDetails = all_data[0]['contentDetails']
+    duration = contentDetails['duration']
+                
+    duration = isodate.parse_duration(duration)
+    durationseconds = duration.total_seconds()
+    video_dur = str(datetime.timedelta(seconds=durationseconds))
+    print(video_dur)
+                
+    totalTimeSeconds = totalTimeSeconds + durationseconds
+    videoProcessed = videoProcessed + 1
+    print("Video", videoProcessed, "out of", len(videoList), "videos processed")
+
 print("Videos counted:", videoCount)
 print("Videos skipped:", videoSkipped)
 
 totalTimeSeconds = round(totalTimeSeconds * retentionFactor)
-print(totalTimeSeconds)
 totalTime = str(datetime.timedelta(seconds=totalTimeSeconds))
-print(totalTime)
+print("Total watch time:", totalTime)
 
 fileGenerated.close()
