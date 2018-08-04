6ddef2e891c10bb15293ddd419fdd22c5c7ed64f
drewv123 <drewv123@10.0.1.33>
2018-08-03 23:47:18 -0700

    gdfg

--- src/main.py
+++ src/main.py
@@ -34,7 +34,10 @@ for lines in fileRead:
                 api_key = "AIzaSyCAExHHkEP_iB9RYmHXlaXC-pjPkj9RWQw"
                 searchUrl = "https://www.googleapis.com/youtube/v3/videos?id=" + videoID + "&key=" + api_key + "&part=contentDetails"
                 response = urllib.request.urlopen(searchUrl)
-                data = json.loads(response)
+                bla = response.read()
+                print(bla)
+                encoding = response.info().get_content_charset('utf-8')
+                data = json.loads(bla.decode(encoding))
                 all_data = data['items']
                 contentDetails = all_data[0]['contentDetails']
                 duration = contentDetails['duration']
